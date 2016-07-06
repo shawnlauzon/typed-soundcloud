fb38eb069bce1257c6d82a1a580f3bc4beedb04c
Homa Wong <homawong@gmail.com>
2016-07-06 10:22:38 -0700

    Update test

--- test/test.ts
+++ test/test.ts
@@ -3,8 +3,19 @@ import test = require('blue-tape');
 import soundcloud = require('soundcloud');
 
 test('soundcloud shape', t => {
-  for (let key in soundcloud) {
-    console.log(`soundcloud exposes ${key}`);
-  }
+  t.assert(soundcloud.initialize, 'soundcloud.initialize exists.');
+  t.assert(soundcloud.get, 'soundcloud.get exists.');
+  t.assert(soundcloud.post, 'soundcloud.post exists.');
+  t.assert(soundcloud.put, 'soundcloud.put exists.');
+  t.assert(soundcloud.delete, 'soundcloud.delete exists.');
+  t.assert(soundcloud.upload, 'soundcloud.upload exists.');
+  t.assert(soundcloud.connect, 'soundcloud.connect exists.');
+  t.assert(soundcloud.isConnected, 'soundcloud.isConnected exists.');
+  t.assert(soundcloud.oEmbed, 'soundcloud.oEmbed exists.');
+  t.assert(soundcloud.resolve, 'soundcloud.resolve exists.');
+  t.assert(soundcloud.Recorder, 'soundcloud.Recorder exists.');
+  t.assert(soundcloud.Promise, 'soundcloud.Promise exists.');
+  t.assert(soundcloud.stream, 'soundcloud.stream exists.');
+  t.assert(soundcloud.connectCallback, 'soundcloud.connectCallback exists.');
   t.end();
 });
