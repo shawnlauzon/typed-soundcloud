b9506673dba719e76b4440eae6c5984558f6a61a
Homa Wong <homawong@gmail.com>
2016-07-06 10:30:08 -0700

    Revert back to interface to avoid `delete` reserve keyword issue.

--- index.d.ts
+++ index.d.ts
@@ -1,37 +1,43 @@
 import { Promise } from 'es6-promise';
+interface SoundCloud {
+  initialize(options: SoundCloud.InitializeOptions);
+  connect(options?: SoundCloud.ConnectOptions): Promise<any>;
 
-export function initialize(options: InitializeOptions);
-export function connect(options?: ConnectOptions): Promise<any>;
+  get(path: string, params?: any): Promise<any>;
+  post(path: string, params?: any): Promise<any>;
+  put(path: string, params?: any): Promise<any>;
+  delete(path: string): Promise<any>;
 
-export function get(path: string, params?: any): Promise<any>;
-export function post(path: string, params?: any): Promise<any>;
-export function put(path: string, params?: any): Promise<any>;
-export function delete(path: string): Promise<any>;
+  upload(options: any): Promise<any>;
+  resolve(url: string): Promise<any>;
+  oEmbed(url: string, params?: any): Promise<any>;
 
-export function upload(options: any): Promise<any>;
-export function resolve(url: string): Promise<any>;
-export function oEmbed(url: string, params?: any): Promise<any>;
-
-export function stream(trackPath: string, secretToken?: string): Promise<Player>;
-
-export interface InitializeOptions {
-  client_id: string;
-  redirect_uri?: string;
-  oauth_token?: string;
+  stream(trackPath: string, secretToken?: string): Promise<SoundCloud.Player>;
 }
 
-export interface ConnectOptions {
-  client_id: string;
-  redirect_uri: string;
-  scope?: string;
+declare namespace SoundCloud {
+  interface InitializeOptions {
+    client_id: string;
+    redirect_uri?: string;
+    oauth_token?: string;
+  }
+
+  interface ConnectOptions {
+    client_id: string;
+    redirect_uri: string;
+    scope?: string;
+  }
+
+  interface Player {
+    play();
+    pause();
+    seek(time: number);
+    currentTime(): number;
+    setVolume(volume: number);
+    getVolume(): number;
+    on(event: string, handler: any);
+  }
 }
 
-export interface Player {
-  play();
-  pause();
-  seek(time: number);
-  currentTime(): number;
-  setVolume(volume: number);
-  getVolume(): number;
-  on(event: string, handler: any);
-}
+declare let SoundCloud: SoundCloud;
+export = SoundCloud;
--- test/test.ts
+++ test/test.ts
@@ -4,18 +4,14 @@ import soundcloud = require('soundcloud');
 
 test('soundcloud shape', t => {
   t.assert(soundcloud.initialize, 'soundcloud.initialize exists.');
+  t.assert(soundcloud.connect, 'soundcloud.connect exists.');
   t.assert(soundcloud.get, 'soundcloud.get exists.');
   t.assert(soundcloud.post, 'soundcloud.post exists.');
   t.assert(soundcloud.put, 'soundcloud.put exists.');
   t.assert(soundcloud.delete, 'soundcloud.delete exists.');
   t.assert(soundcloud.upload, 'soundcloud.upload exists.');
-  t.assert(soundcloud.connect, 'soundcloud.connect exists.');
-  t.assert(soundcloud.isConnected, 'soundcloud.isConnected exists.');
-  t.assert(soundcloud.oEmbed, 'soundcloud.oEmbed exists.');
   t.assert(soundcloud.resolve, 'soundcloud.resolve exists.');
-  t.assert(soundcloud.Recorder, 'soundcloud.Recorder exists.');
-  t.assert(soundcloud.Promise, 'soundcloud.Promise exists.');
+  t.assert(soundcloud.oEmbed, 'soundcloud.oEmbed exists.');
   t.assert(soundcloud.stream, 'soundcloud.stream exists.');
-  t.assert(soundcloud.connectCallback, 'soundcloud.connectCallback exists.');
   t.end();
 });
